8d02b3b8f15d9fa558a438e6136a70e1b64531b3
BenjaminSpoiden <spoiden.benjamin@hotmail.com>
2021-06-28 19:29:35 +0200

    removing small detail

--- components/BorderCountry.js
+++ components/BorderCountry.js
@@ -4,16 +4,12 @@ import Link from "next/link"
 export const BorderCountry = ({ country }) => {
     
     return (
-        <>
-            <Link href={`/${encodeURIComponent(country.toLowerCase())}`} >
-                <a>
-                    <div className="flex flex-row px-1 py-2 dark:bg-darkGray cursor-pointer bg-white shadow justify-center w-32 ">
-                        <div>
-                            <p className="font-nunito dark:text-gray-400 text-xs">{country}</p>
-                        </div>
-                    </div>
-                </a>
-            </Link>
+        <>  
+            <div className="flex flex-row px-1 py-2 dark:bg-darkGray cursor-pointer bg-white shadow justify-center w-32 ">
+                <div>
+                    <p className="font-nunito dark:text-gray-400 text-xs">{country}</p>
+                </div>
+            </div>
         </>
     )
 }
